b16c1b8c8f0becce0c1e79c82d24c406c6e6900e
Kiszel Daniel <kiszeldaniel@gmail.com>
2023-08-28 13:15:27 +0200

    asd

--- src/components/SearchBar/SearchResult/SearchResult.tsx
+++ src/components/SearchBar/SearchResult/SearchResult.tsx
@@ -4,21 +4,19 @@ import "./SearchResult.css";
 
 const SearchResult = (props: SearchResultProps) => {
 
-    const handleClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        const target = event.target as HTMLOptionElement;
-        console.log(target);
+    const handleClick = (e: React.MouseEvent<HTMLOptionElement>) => {
+        const target = e.target as HTMLOptionElement;
         const value = target.text;
-        console.log('asd')
         props.onResult(value);
     };
-  
+    
     return (
         <div className="result-wrapper">
         {props.cities.length != 0 ? (
             <div className="search-result-wrapper">
             {props.cities.map((item, index) => {
                 return (
-                <option key={index} className="search-result" onClick={handleClick}>
+                <option key={index} className="search-result" onClick={handleClick} >
                     {item.name}
                 </option>
                 );
