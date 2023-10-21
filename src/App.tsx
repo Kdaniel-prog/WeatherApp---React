a609a22e74a6fabfb672714f451214ad03a52d71
Kiszel Daniel <kiszeldaniel@gmail.com>
2023-10-21 15:25:41 +0200

    bounce too many request

--- src/App.tsx
+++ src/App.tsx
@@ -9,18 +9,13 @@ import "./App.css";
 
 function App() {
   const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isEmpty, setIsEmpty] = useState(true);
   const [apiUrl, setApiUrl] = useState(`http://api.weatherapi.com/v1/forecast.json?key=${TOKEN}&q=Budapest&days=1&aqi=no&alerts=no`);
 
   const fetchData = useCallback(async () => {
     try {
-      setIsLoading(true);
       const response = await fetch(apiUrl);
       const data = await response.json();
       setWeatherData(data);
-      weatherData === null ? setIsEmpty(false) : setIsEmpty(true);
-      setIsLoading(false);
     } catch (error) {
       console.log(error)
     }
--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,161 +1,178 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import Card from "../UI/Card/Card";
 import "./SearchBar.css";
 import CitiesProps from "./CitiesProps";
 import SearchResult from "./SearchResult/SearchResult";
 import SearchBarProps from "./SearchBarProps";
 import TOKEN from "../../api/apiWeatherProps/Token";
+import { debounce } from "lodash"
 
 
 const SearchBar = (props: SearchBarProps) => {
-    const [cities, setCities] = useState<CitiesProps[] | []>([]);
-    const [searchValue, setSearchValue] = useState<string>('');
-    const [isLoading, setIsLoading] = useState(false);
-    const [isEmpty, setIsEmpty] =useState(true);
-    const [showResult, setShowResult] =useState(false);
-    const [isMouseIn, setIsMouseIn] = useState(false);
-    const [apiUrl, setApiUrl] = useState('');
-    const [index, setIndex] = useState(0);
-    const [enterPressed, setEnterPressed] = useState(false);
+  const [cities, setCities] = useState<CitiesProps[] | []>([]);
+  const [searchValue, setSearchValue] = useState<string>("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isEmpty, setIsEmpty] = useState(true);
+  const [showResult, setShowResult] = useState(false);
+  const [isMouseIn, setIsMouseIn] = useState(false);
+  const [index, setIndex] = useState(0);
+  const [enterPressed, setEnterPressed] = useState(false);
 
-     const fetchData = async () => {
-        try {
-            setApiUrl(`http://api.weatherapi.com/v1/search.json?key=${TOKEN}&q=${searchValue}`);
-            setIsLoading(true);
-            const response = await fetch(apiUrl);
-            const data = await response.json();
-            setCities(data);
-            cities.length === 0 ? setIsEmpty(true) : setIsEmpty(false);
-            setIsLoading(false);
-            if(!isEmpty){
-                setShowResult(true);
-            }
-        } catch (error) {
-            console.log(error)
-        }
+  const fetchData = useCallback(async (apiUrl: string) => {
+    try {
+      setIsLoading(true);
+      const response = await fetch(apiUrl);
+      console.log(apiUrl)
+      const data = await response.json();
+      data ? setIsEmpty(false) : setIsEmpty(true);
+      setIsLoading(false);
+      return data;
+    } catch (error) {
+      console.log(error);
+      console.log("error");
     }
-    
-    const searchCities = async (event: React.ChangeEvent<HTMLInputElement>) => {
-        setIsLoading(true);
-        setSearchValue(event.target.value);
-        if(searchValue){
-            fetchData();
-        }
+  }, []);
+
+  const searchCities = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setSearchValue(value);
+    if (value && value.length > 2) {
+        debouncedSearch(`http://api.weatherapi.com/v1/search.json?key=${TOKEN}&q=${value}`);
+    }
+  };
+
+  const debouncedSearch = React.useRef(
+    debounce(async (criteria) => {
+      setCities(await fetchData(criteria));
+    }, 500)
+  ).current;
+
+  React.useEffect(() => {
+    return () => {
+      debouncedSearch.cancel();
     };
-    
-    const handleKey = (event: React.KeyboardEvent<HTMLInputElement>) => {
-        const keys =["ArrowUp", "ArrowDown", "Enter"];
-        if(!keys.includes(event.code)){
-            return;
-        }
+  }, [debouncedSearch]);
 
-        if(event.code === 'Enter'){
-            event.preventDefault();
-            setEnterPressed(true);
-            return;
-        }
-        
-        if(event.code === 'ArrowUp'){
-            if(index > 1 && index <= cities.length ){
-                setIndex(index - 1);
-            }
-            if(index === 1 || index === 0){
-                setIndex(5);
-            }
-            return;
-        }
-
-        if(event.code === 'ArrowDown'){
-            if(index === cities.length){
-                setIndex(1);
-            }
-            if(index >= 0 && index < cities.length){
-                setIndex(index + 1);
-            }
-        }
+  const handleKey = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    const keys = ["ArrowUp", "ArrowDown", "Enter"];
+    if (!keys.includes(event.code)) {
+      return;
     }
 
-    const handleResult = (value: string) =>{
-        const newCity = cities.find(c=> {
-            if(c.name == value){
-                return c;
-            }
-        });
-        
-        if(newCity){
-            setCities([{...newCity}]);
-        }
-
-        setSearchValue(value);
-        setShowResult(false);
-        props.onResult(value);
+    if (event.code === "Enter") {
+      event.preventDefault();
+      setEnterPressed(true);
+      return;
     }
 
-    const showSearchResult = () => {
-        setShowResult(true);
+    if (event.code === "ArrowUp") {
+      if (index > 1 && index <= cities.length) {
+        setIndex(index - 1);
+      }
+      if (index === 1 || index === 0) {
+        setIndex(5);
+      }
+      return;
     }
 
-    const hideSearchResult = () =>{
-        if(!isMouseIn){
-            setShowResult(false);
-        }
+    if (event.code === "ArrowDown") {
+      if (index === cities.length) {
+        setIndex(1);
+      }
+      if (index >= 0 && index < cities.length) {
+        setIndex(index + 1);
+      }
+    }
+  };
+
+  const handleResult = (value: string) => {
+    const newCity = cities.find((c) => {
+      if (c.name == value) {
+        return c;
+      }
+    });
+
+    if (newCity) {
+      setCities([{ ...newCity }]);
+    }else{
+        setCities([]);
     }
 
-    const checkMouseIn = () =>{
-        setIsMouseIn(true);
+    setSearchValue(value);
+    setShowResult(false);
+    props.onResult(value);
+  };
+
+  const showSearchResult = () => {
+    setShowResult(true);
+  };
+
+  const hideSearchResult = () => {
+    if (!isMouseIn) {
+      setShowResult(false);
+    }
+  };
+
+  const checkMouseIn = () => {
+    setIsMouseIn(true);
+  };
+
+  const checkMouseOut = () => {
+    setIsMouseIn(false);
+  };
+
+  const handleEnter = (value: string) => {
+    const newCity = cities.find((c) => {
+      if (c.name == value) {
+        return c;
+      }
+    });
+
+    if (newCity) {
+      setCities([{ ...newCity }]);
     }
 
-    const checkMouseOut = () =>{
-        setIsMouseIn(false);
-    }
+    setEnterPressed(false);
+    setIndex(0);
+    setSearchValue(value);
+    setShowResult(false);
+    props.onResult(value);
+  };
 
-    const handleEnter = (value:string) =>{
-        const newCity = cities.find(c=> {
-            if(c.name == value){
-                return c;
-            }
-        });
-        
-        if(newCity){
-            setCities([{...newCity}]);
-        }
+  const handleRessetEnter = () => {
+    setEnterPressed(false);
+  };
 
-        setEnterPressed(false);
-        setIndex(0);
-        setSearchValue(value);
-        setShowResult(false);
-        props.onResult(value);
-    }
-
-    const handleRessetEnter = () =>{
-        setEnterPressed(false);
-    }
-
-    return (
-        <div onBlur={hideSearchResult} onMouseEnter={checkMouseIn} onMouseLeave={checkMouseOut}>
-            <Card className="card--align">
-                <input
-                    type="text"
-                    value={searchValue}
-                    placeholder="Search..."
-                    onKeyUp={handleKey}
-                    onChange={searchCities}
-                    onClick={showSearchResult}
-                ></input>
-            </Card>
-            <SearchResult 
-                cities={cities}
-                show={showResult}
-                index={index}
-                enterPressed={enterPressed}
-                searchValue={searchValue} 
-                isEmpty={isEmpty} 
-                isLoading={isLoading}
-                onRessetEnter={handleRessetEnter}
-                onEnterPressed={handleEnter} 
-                onResult={handleResult}/>
-        </div>
-    );
+  return (
+    <div
+      onBlur={hideSearchResult}
+      onMouseEnter={checkMouseIn}
+      onMouseLeave={checkMouseOut}
+    >
+      <Card className="card--align">
+        <input
+          type="text"
+          value={searchValue}
+          placeholder="Search..."
+          onKeyUp={handleKey}
+          onChange={searchCities}
+          onClick={showSearchResult}
+        ></input>
+      </Card>
+      <SearchResult
+        cities={cities}
+        show={showResult}
+        index={index}
+        enterPressed={enterPressed}
+        searchValue={searchValue}
+        isEmpty={isEmpty}
+        isLoading={isLoading}
+        onRessetEnter={handleRessetEnter}
+        onEnterPressed={handleEnter}
+        onResult={handleResult}
+      />
+    </div>
+  );
 };
 
 export default SearchBar;
