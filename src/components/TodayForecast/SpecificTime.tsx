f2c42457e4ac532733b7731bf77e733901d08107
Kiszel Daniel <74417423+Kdaniel-prog@users.noreply.github.com>
2024-01-07 13:25:13 +0100

    Update SpecificTime.tsx

--- src/components/TodayForecast/SpecificTime.tsx
+++ src/components/TodayForecast/SpecificTime.tsx
@@ -4,9 +4,9 @@ import Col from 'react-bootstrap/Col';
 import classes from './SpecificTime.module.css';
 
 const SpecificTime = (props: SpecificTimeProps) => {
-    console.log(props.time.toLocaleTimeString())
+    console.log(props.time.toISOString().slice(0, 19).replace('T', ' '))
     // Convert the string to a Date object
-    const time = new Date(props.time.toDateString().replace(/-/g, '-' ));
+    const time = new Date(props.time.toISOString().slice(0, 19).replace('T', ' '));
     
 
     // Check if the conversion was successful
