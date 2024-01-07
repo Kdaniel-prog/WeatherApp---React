152a16ef169a2cb4c6c8f7774ac4fd4329a40222
Kiszel Daniel <74417423+Kdaniel-prog@users.noreply.github.com>
2024-01-07 13:06:33 +0100

    Update SpecificTime.tsx

--- src/components/TodayForecast/SpecificTime.tsx
+++ src/components/TodayForecast/SpecificTime.tsx
@@ -6,7 +6,7 @@ import classes from './SpecificTime.module.css';
 const SpecificTime = (props: SpecificTimeProps) => {
 
     // Convert the string to a Date object
-    const time = new Date(props.time.toString().replace(/-/g, '-' ));
+    const time = new Date(props.time.toString().replace(/-/g, '-' ).replace(' ', 'T'));
 
     // Check if the conversion was successful
     if (isNaN(time.getTime())) {
